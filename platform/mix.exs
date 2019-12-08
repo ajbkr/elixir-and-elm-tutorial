b6c3aec938e40ab664cd01b4c573dfaf5b8a11a8
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-12-08 13:15:59 +0000

    Add registration changeset

--- platform/mix.exs
+++ platform/mix.exs
@@ -41,7 +41,9 @@ defmodule Platform.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:comeonin, "~> 4.1"},
+      {:bcrypt_elixir, "~> 1.1"}
     ]
   end
 
